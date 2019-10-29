1ca2870d287ef720dcd5776383172aba86f557f8
LBoisseron <kalaloolb@gmail.com>
2019-10-29 15:51:38 -0400

    w du 29/10/19
    ACTUNEWS

---
+++ 08-ACTUNEWS/page-exemple.php
@@ -0,0 +1,25 @@
+<?php
+// inclusion de header.php sur la page
+require_once(__DIR__.'/partials/header.php');
+?>
+
+<!--ICI viendra le contenu de la page-->
+<div class="p-3 mx-auto text-center">
+    <h1 class="display-4">BanMwenFréLa</h1>
+</div>
+
+<div class="py-5 bg-light">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>KontanVwèZot</h1>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php
+// inclusion de footer.php sur la page
+require_once(__DIR__.'/partials/footer.php');
+?>
---
+++ 08-ACTUNEWS/partials/header.php
@@ -0,0 +1,49 @@
+<?php
+$categories = ['ManjéBon', 'KréyolVaybz', 'KeepLeSwag', 'Lyannaj']
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>BanMwenFréLa | Lokal lokal</title>
+
+    <!--BOOTSTRAP CSS-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
+    <!--STYLES PERSONNALISES-->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+<body>
+
+<!--MENU DU SITE-->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">BanMwenFréLa</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">KontanVwèZot <span class="sr-only">(current)</span></a>
+                </li>
+<!--Les caégories du site-->
+                <?php foreach($categories as $categorie) { ?>
+                <li class="nav-item active">
+                <a class="nav-link" href="#"><?= $categorie ?><span class="sr-only">(current)</span></a>
+                </li>
+                <?php } ?>
+                <li class="nav-item active btn-outline-primary mx-2">
+                    <a class="nav-link" href="#">Konèkté'w <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active btn-outline-primary mx-2">
+                    <a class="nav-link" href="#">Enskri'w <span class="sr-only">(current)</span></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+<!--FIN DU MENU DU SITE-->
+    
